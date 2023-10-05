5dedd6f94c626153d1b1a26b7b3552de6fdcb73e
Dmitrii Kurtsev <dmit-vuk@mail.ru>
2023-10-05 20:32:24 +0300

    pre-commit add

--- MLOps/mlops/infer.py
+++ MLOps/mlops/infer.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import torch
-from torch import nn
-
 from models import MLPNN
-from train import get_dataloader,  evaluate
+from torch import nn
+from train import evaluate, get_dataloader
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_loader = get_dataloader(batch_size=512, train=False)
 
-    model = MLPNN(in_features=784, hidden_sizes = [1024, 512, 128, 10])
-    model.load_state_dict(torch.load('best_params.pth'))
+    model = MLPNN(in_features=784, hidden_sizes=[1024, 512, 128, 10])
+    model.load_state_dict(torch.load("best_params.pth"))
     loss = nn.CrossEntropyLoss()
-    test_loss, test_acc , y_pred, y_true = evaluate(dataloader=test_loader, model=model, loss_fn=loss, device='cpu')
-    print('Test loss: ', test_loss)
-    print('Test accuracy: ', test_acc.item())
-    
+    test_loss, test_acc, y_pred, y_true = evaluate(
+        dataloader=test_loader, model=model, loss_fn=loss, device="cpu"
+    )
+    print("Test loss: ", test_loss)
+    print("Test accuracy: ", test_acc.item())
+
     y_pred = y_pred.numpy()
     y_true = y_true.numpy()
-    df = pd.DataFrame({'true': y_true, 'pred': y_pred}) #convert to a dataframe
+    df = pd.DataFrame({"true": y_true, "pred": y_pred})
     df.to_csv("predictions.csv", index=False)
--- MLOps/mlops/models.py
+++ MLOps/mlops/models.py
@@ -1,19 +1,16 @@
-import torch
-from torch import nn
-import torchvision
-
 from typing import List
 
+import torch
+import torchvision
+from torch import nn
+
+
 class MLPNN(nn.Module):
-    def __init__(
-        self,
-        in_features: int,
-        hidden_sizes: List[int]
-    ):
+    def __init__(self, in_features: int, hidden_sizes: List[int]):
         super(MLPNN, self).__init__()
 
         self.flatten = nn.Flatten()
         self.mlp_nn = torchvision.ops.MLP(in_features, hidden_sizes)
-        
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.mlp_nn(self.flatten(x))
